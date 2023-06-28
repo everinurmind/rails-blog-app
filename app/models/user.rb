015550ba2602de7f6a3b7e2bbebfee43bb0ed8d7
everinurmind <nurbol.sultanov@gmail.com>
2023-06-28 07:14:17 -0600

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :photo
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
