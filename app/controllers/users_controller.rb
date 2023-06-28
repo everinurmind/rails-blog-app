f52e29d34940f57e6e913acd9a8ac67f58292289
everinurmind <nurbol.sultanov@gmail.com>
2023-06-28 07:14:10 -0600

    Add delete user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.photo = params[:user][:photo] if params[:user][:photo].present?
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: 'User successfully deleted.'
+  end
 end
