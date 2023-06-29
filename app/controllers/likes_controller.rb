be0d53c9d4ab4ed7b5360a780ec6e1bd448614b2
everinurmind <nurbol.sultanov@gmail.com>
2023-06-29 13:36:42 -0600

    Fix linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,14 @@
 class LikesController < ApplicationController
-    def create
-      @post = Post.find(params[:post_id])
-      @like = @post.likes.build
-      @like.author = current_user
-      @like.user_id = current_user.id
-  
-      if @like.save
-        redirect_to user_post_path(@post.author, @post), notice: 'Like added successfully.'
-      else
-        redirect_to user_post_path(@post.author, @post), alert: 'Failed to add like.'
-      end
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build
+    @like.author = current_user
+    @like.user_id = current_user.id
+
+    if @like.save
+      redirect_to user_post_path(@post.author, @post), notice: 'Like added successfully.'
+    else
+      redirect_to user_post_path(@post.author, @post), alert: 'Failed to add like.'
     end
   end
-  
+end
