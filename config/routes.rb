93bdc633e8859be7cf3c26e73671ecf15b101518
everinurmind <nurbol.sultanov@gmail.com>
2023-06-29 15:10:01 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :posts do
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :new]
       resources :likes, only: [:create]
     end
 
