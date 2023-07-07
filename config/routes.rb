b05f9dbbf2f5598c824460815255e52b30e9bda4
everinurmind <nurbol.sultanov@gmail.com>
2023-07-07 16:57:53 -0600

    Update routes to point to the Devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
 
+  devise_for :users
+
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
