3b348430da6fc027b238c9c6caba8c6b204e39e5
everinurmind <nurbol.sultanov@gmail.com>
2023-07-07 19:02:18 -0600

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
-  end  
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   def after_sign_out_path_for(_resource)
     root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#index'
-
-  devise_for :users
+  root to: 'users#index'
+  devise_for :users, path: 'auth',
+                     path_names: { sign_in: 'login', password: 'secret', sign_up: 'register' }
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy', as: :logout
+  end
 
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
